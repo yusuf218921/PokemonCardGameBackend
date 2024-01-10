d3950fb16f9314e15edc039ef1ce8e111b1c0209
yusuf sonmez <yusufsonmez951@gmail.com>
2024-01-10 15:11:54 +0300

    EntityRepository İmpl

---
+++ Core/DataAccess/Concrete/EntityFramework/Context/RepositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concrete.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Concrete.EntityFramework.Context
+{
+    public class RepositoryContext : DbContext
+    {
+        DbSet<User> Users { get; set; } = null!;
+
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
